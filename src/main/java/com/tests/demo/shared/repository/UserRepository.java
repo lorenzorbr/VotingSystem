785add296d4b295245cc4a2ca6c5755c70fce23d
Lorenzo Bragagnolo <40443452+lorenzorbr@users.noreply.github.com>
2021-01-19 18:20:23 -0300

    Delete src directory

--- src/main/java/com/tests/demo/shared/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tests.demo.shared.repository;
-
-import com.tests.demo.shared.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
